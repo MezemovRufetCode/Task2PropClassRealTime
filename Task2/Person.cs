4a42378fbe1bc92b0b6817d9d6ff0e525361b267
MezemovRufetCode <rufatam@code.edu.az>
2021-10-03 23:26:24 +0400

    Add project files.

---
+++ Task2/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int BirthdayYear { get; set; }
+
+        public Person(string Name,string Surname,int Year)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.BirthdayYear = Year;
+        }
+
+        public void Find()
+        {
+            DateTime time = DateTime.Now;
+            int year = time.Year;
+            BirthdayYear = year - BirthdayYear;
+            Console.WriteLine($"{Name} {Surname} {BirthdayYear}");
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            int year = time.Year;
+
+            Console.WriteLine("Add person's name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Add person's surname");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Add person year of birth:");
+            int n = int.Parse(Console.ReadLine());
+            if (n > year)
+            {
+                Console.WriteLine("It can not be!!!");
+                return;
+            }
+            Person p1 = new Person(name,surname,n);
+            p1.Find();
+        }
+    }
+}
